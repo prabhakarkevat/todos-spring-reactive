7dd8ffce43ac83a406f5ed4186456f325a04d68b
Prabhakar Kevat <prabhakar.kevat@crossasyst.com>
2023-04-15 11:51:10 +0530

    initial commit

---
+++ src/main/java/com/example/todoreactive/TodoReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todoreactive/TodoReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class TodoReactiveApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
